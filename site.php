c55220c90a975b7d976d9ee5f14f93a2e3291e47
anderson-psv <andersonpintodasilva@hotmail.com>
2020-10-30 14:59:55 -0300

    Adicionado páginação de Produtos
    Adicionado na parte as categorias.

--- site.php
+++ site.php
@@ -16,15 +16,32 @@ $app->get('/', function()
 
 $app->get("/categories/:idcategory", function($idcategory)
 {
+	$currentPage = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
+
+	$pagination = $category->getProductsPage($currentPage);
+
+	$pages = [];
+
+	for ($i=1; $i<=$pagination['pages'] ; $i++)
+	{ 
+		array_push($pages, [
+			'link'=>"/categories/{$category->getidcategory()}?page={$i}",
+			'page'=>$i
+		]);
+	}
 
 	$page = new Page;
 
 	$page->setTpl("category", [
 		"category"=>$category->getValues(),
-		"products"=>Product::checkList($category->getProducts())
+		"products"=>$pagination['data'],
+		"pages"=>$pages,
+		"currentpage"=>$currentPage,
+		"lastpage"=>$pagination['pages']
 	]);
 });
 
--- views-cache/category.6a64b51768f04e8ef50ff8dec9ea4d12.rtpl.php
+++ views-cache/category.6a64b51768f04e8ef50ff8dec9ea4d12.rtpl.php
@@ -40,21 +40,31 @@
                 <div class="product-pagination text-center">
                     <nav>
                         <ul class="pagination">
+                        <?php if( $currentpage > 1 ){ ?>
+
                         <li>
-                            <a href="#" aria-label="Previous">
+                            <a href="/categories/<?php echo htmlspecialchars( $category["idcategory"], ENT_COMPAT, 'UTF-8', FALSE ); ?>?page=<?php echo htmlspecialchars( $currentpage - 1, ENT_COMPAT, 'UTF-8', FALSE ); ?>" aria-label="Previous">
                             <span aria-hidden="true">«</span>
                             </a>
                         </li>
-                        <li><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
+                        <?php } ?>
+
+
+                        <?php $counter1=-1;  if( isset($pages) && ( is_array($pages) || $pages instanceof Traversable ) && sizeof($pages) ) foreach( $pages as $key1 => $value1 ){ $counter1++; ?>
+
+                        <li><a href="<?php echo htmlspecialchars( $value1["link"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["page"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></li>
+                        <?php } ?>
+
+
+                        <?php if( $currentpage != $lastpage ){ ?>
+
                         <li>
-                            <a href="#" aria-label="Next">
+                            <a href="/categories/<?php echo htmlspecialchars( $category["idcategory"], ENT_COMPAT, 'UTF-8', FALSE ); ?>?page=<?php echo htmlspecialchars( $currentpage + 1, ENT_COMPAT, 'UTF-8', FALSE ); ?>" aria-label="Next">
                             <span aria-hidden="true">»</span>
                             </a>
                         </li>
+                        <?php } ?>
+
                         </ul>
                     </nav>                        
                 </div>
