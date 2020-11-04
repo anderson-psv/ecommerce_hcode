a3957198eee3826e80f5e2e07ea6ee2cdf852494
anderson-psv <andersonpintodasilva@hotmail.com>
2020-11-04 09:11:20 -0300

    Classe e layout do carrinho
    Adicionado template e funcções de salvar e sessão do carrinho.

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -12,6 +12,45 @@ class User extends Model
     const SECRET   = "HcodePhp7_Secret";
     const SECRET2  = "HcodePHp7_NotSec";
 
+    public static function getFromSession()
+    {
+        $user = new User();
+
+        if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0)
+        {
+            $user->setData($_SESSION[User::SESSION]);
+        }
+
+        return $user;
+    }
+
+    public static function checkLogin($inadmin = true)
+    {
+        if(!isset($_SESSION[User::SESSION])
+            ||
+            !$_SESSION[User::SESSION]
+            || 
+            !(int)$_SESSION[User::SESSION]['iduser'] > 0)
+        {
+            return false;
+        }
+        else
+        {
+            if($inadmin === true && (bool)$_SESSION[User::SESSION]['inadmin'])
+            {
+                return true;
+            }
+            else if($inadmin === false)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
     public static function login($login, $password) 
     {
         $sql = new Sql();
@@ -46,13 +85,7 @@ class User extends Model
 
     public static function verifyLogin($inadmin = true)
     {
-        if(!isset($_SESSION[User::SESSION])
-            ||
-            !$_SESSION[User::SESSION]
-            || 
-            !(int)$_SESSION[User::SESSION]['iduser'] > 0 
-            ||
-            (bool)$_SESSION[User::SESSION]['inadmin'] !== $inadmin)
+        if(User::checkLogin($inadmin))
         {
             header("Location: /admin/login");
             exit;
