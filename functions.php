e48cd087cc124ae2d975758a0c112c24950df08c
anderson-psv <andersonpintodasilva@hotmail.com>
2020-11-10 16:02:49 -0300

    Meus Pedidos
    Adicionado template e funcionalidade para visualizar pedidos realizados.

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
 
+use \Hcode\Model\Cart;
 use \Hcode\Model\User;
+
 
 function formatPrice($vlprice)
 {
@@ -21,6 +23,24 @@ function getUserName()
 {
     $user = User::getFromSession();
     return $user->getdesperson();
+}
+
+function getCartNrQtd()
+{
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return $totals['nrqtd'];
+}
+
+function getCartVlSubTotal()
+{
+    $cart = Cart::getFromSession();
+
+    $totals = $cart->getProductsTotals();
+
+    return formatPrice($totals['vlprice']);
 }
 
 ?>
